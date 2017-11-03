86112156577d970bdca02bd555b0ca959a0a3330
Kelsey Langlois <kels.langlois@gmail.com>
2017-11-03 09:03:38 -0700

    Update player list to use player service.

--- src/app/player-list/player-list.component.ts
+++ src/app/player-list/player-list.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseListObservable } from 'angularfire2/database';
+
+import { Player } from '../player.model';
+import { PlayerService } from '../player.service';
 
 @Component({
   selector: 'app-player-list',
   templateUrl: './player-list.component.html',
-  styleUrls: ['./player-list.component.css']
+  styleUrls: ['./player-list.component.css'],
+  providers: [PlayerService]
 })
 export class PlayerListComponent implements OnInit {
+  players: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private playerService: PlayerService) { }
 
   ngOnInit() {
+    this.players = this.playerService.getPlayers();
   }
 
 }
